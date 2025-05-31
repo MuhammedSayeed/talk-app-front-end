06553edecc9496e660cec33718e936c2d433807d
Mohamed sayed <mhmdsayed90031@gmail.com>
2025-05-31 13:10:54 +0300

    solve search card resposnive

--- src/components/search/UserCard.tsx
+++ src/components/search/UserCard.tsx
@@ -3,11 +3,13 @@ import useUserProfileStore from "@/lib/store/UserProfileStore";
 import Avatar from "../ui/Avatar"
 import { ISearchResults } from "@/interfaces/search";
 import Card from "../ui/Card";
+import useUtilts from "@/hooks/useUtilts";
 interface IProps {
     onClose: () => void;
     user: ISearchResults
 }
 const UserCard = ({ user, onClose: onCloseSearchModal }: IProps) => {
+    const { truncateText } = useUtilts();
     const { setId, toggleUserProfile } = useUserProfileStore();
     const handleOnClick = () => {
         onCloseSearchModal();
@@ -20,7 +22,7 @@ const UserCard = ({ user, onClose: onCloseSearchModal }: IProps) => {
                 <Avatar image={user?.profilePic?.src} name="dummy" />
                 <div className="flex flex-col justify-end font-medium">
                     <span className="text-gray-200">{user.name}</span>
-                    <span className="text-sm text-gray-400">{user.username}</span>
+                    <span className="text-sm text-gray-400">{truncateText(user.username, 20)}</span>
                 </div>
             </div>
         </Card>
